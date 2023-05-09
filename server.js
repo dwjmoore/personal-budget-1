06b13d39c2013e3e106dc2c89533c838894d1d10
David Moore <dwjmoore@gmail.com>
2023-05-09 21:25:36 +0000

    added array of envelope objects

--- server.js
+++ server.js
@@ -2,8 +2,28 @@ const express = require('express');
 
 const app = express();
 
+const envelopes = [
+	{
+		id: 1,
+		title: 'Rent',
+		budget: 1500
+	},
+	{
+		id: 2,
+		title: 'Groceries',
+		budget: 700
+	},
+	{
+		id: 3,
+		title: 'Car',
+		budget: 300
+	}
+];
+
+let newID = 4;
+
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.send('Welcome to the budgeting API.');
 });
 
 app.listen(3000, () => console.log('server started'));
